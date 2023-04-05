863cfcb44b09a2e8b07da0ed981d3c7b6657cc1d
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-05 22:01:25 +0530

    05.04.2023

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('contact/',views.contact, name='contact')
+    path('contact/',views.contact, name='contact'),
+    path('signup/',views.signup, name='signup'),
+    path('login/',views.login, name='login')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,9 @@ def index(request):
 
 def contact(request):
 	return render(request,'contact.html')
+
+def signup(request):
+	return render(request,'signup.html')
+
+def login(request):
+	return render(request,'login.html')
