fa541148f541402717ed188300ad7f792ac75fdd
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-05 23:36:22 +0530

    05.04.2023

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import Contact
 
 # Create your views here.
 
@@ -7,7 +8,17 @@ def index(request):
 
 
 def contact(request):
-	return render(request,'contact.html')
+	if request.method=="POST":
+		Contact.objects.create(
+			name=request.POST['name'],
+			email=request.POST['email'],
+			mobile=request.POST['mobile'],
+			remarks=request.POST['remarks']
+			)
+		msg="Contact Saved Successfully ,,!"
+		return render(request,'contact.html',{'msg':msg})
+	else:
+		return render(request,'contact.html')
 
 def signup(request):
 	return render(request,'signup.html')
