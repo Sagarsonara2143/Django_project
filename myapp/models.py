85bfd2d19a37e7042a4db8cf7cdbda004ba266a4
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-06 21:11:07 +0530

    06.04.2023

--- myapp/models.py
+++ myapp/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
 	name=models.CharField(max_length=100)
 	email=models.EmailField()
 	mobile=models.PositiveSmallIntegerField()
-	remaks=models.TextField()	
+	remarks=models.TextField()	
 
 	def __str__(self):
 		return self.name
