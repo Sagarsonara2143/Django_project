31cc4d5d09af993bba70c5d6caebf5f1cab06625
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-06 21:49:26 +0530

    06.04.2023

--- myapp/views.py
+++ myapp/views.py
@@ -15,10 +15,12 @@ def contact(request):
 			mobile=request.POST['mobile'],
 			remarks=request.POST['remarks']
 			)
+		contacts=Contact.objects.all()
 		msg="Contact Saved Successfully ,,!"
-		return render(request,'contact.html',{'msg':msg})
+		return render(request,'contact.html',{'msg':msg,'contacts':contacts})
 	else:
-		return render(request,'contact.html')
+		contacts=Contact.objects.all()
+		return render(request,'contact.html',{'contacts':contacts})
 
 def signup(request):
 	return render(request,'signup.html')
