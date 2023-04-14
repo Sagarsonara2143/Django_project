52a853d8fe0bb18d39bdd258b38aaddefdde0275
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-14 23:53:33 +0530

    14.04.2023

--- CRUD/myapp/models.py
+++ CRUD/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Staff(models.Model):
+	category = (
+		('trainer','trainer'),
+		('counsellor','counsellor'),
+		('backoffice','backoffice'),
+		('admin','admin')
+		)
+
+	staff_cat=models.CharField(max_length=100,choices=category)
+	fname=models.CharField(max_length=100)
+	lname=models.CharField(max_length=100)
+	email=models.EmailField()
+	mobile=models.PositiveSmallIntegerField()
+
+	def __str__(self):
+		return self.fname+" - "+self.staff_cat
