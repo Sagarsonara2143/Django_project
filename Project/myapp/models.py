bd9066e6ea09f892f2761bcf21c3de60f58390e7
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-04-24 19:47:14 +0530

    24.04.2023

--- Project/myapp/models.py
+++ Project/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	fname=models.CharField(max_length=100)
+	lname=models.CharField(max_length=100)
+	email=models.EmailField()
+	mobile=models.PositiveSmallIntegerField()
+	address=models.TextField(max_length=100)
+	city=models.CharField(max_length=100)
+	zipcode=models.PositiveSmallIntegerField()
+	password=models.CharField(max_length=100)
+	
+	def __str__(self):
+		return self.fname+" "+self.lname
--- Project/myapp/views.py
+++ Project/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
@@ -6,4 +7,31 @@ def index(request):
 	return render(request,'index.html')
 
 def signup(request):
-	return render(request,'signup.html')
+	if request.method=="POST":
+		try:
+			User.objects.get(email=request.POST['email'])
+			msg="Email Id is already registered"
+			return render(request,'signup.html',{'msg':msg})
+		except:
+			if request.POST['password']==request.POST['cpassword']:
+				User.objects.create(
+					fname=request.POST['fname'],
+					lname=request.POST['lname'],
+					email=request.POST['email'],
+					mobile=request.POST['mobile'],
+					address=request.POST['address'],
+					city=request.POST['city'],
+					zipcode=request.POST['zipcode'],
+					password=request.POST['password'],
+					)
+				msg="User Sign Up Successfully"
+				return render(request,'login.html',{'msg':msg})
+			else:
+				msg="Password and Confirm password does not matched"
+				return render(request,'signup.html',{'msg':msg})
+	else:
+		return render(request,'signup.html')
+
+
+def login(request):
+	return render(request,'login.html')
