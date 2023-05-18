4611c653fed2f93e701a162207008875aaaf30d2
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-18 21:34:40 +0530

    18.05.2023

--- Mini project - Django Product Management/myapp/views.py
+++ Mini project - Django Product Management/myapp/views.py
@@ -16,6 +16,7 @@ def product_add(request):
 			)	
 		msg="Product Added Successfully"
 		product=Product_master.objects.all()
-		return render(request,"product-master.html",{'msg':msg,'product':product})
+		print(product)
+		return render(request,'product-master.html',{'msg':msg,'product':product})
 	else:
 		return render(request,"product-master.html")
