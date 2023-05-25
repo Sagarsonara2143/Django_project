10d973fae0d33f4a3e0abf3e78f762b8b8bc4ef1
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-25 21:54:06 +0530

    25.05.2023

---
+++ Mini Digital Society/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.mail import send_mail
+import random
+
+
+# Create your views here.
+
+def login(request):
+	return render(request,'login.html')
