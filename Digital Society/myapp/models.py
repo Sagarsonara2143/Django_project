dbc735f3a66d5709f3f535275b15658d1da2eedd
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-29 18:41:52 +0530

    29.05.2023

--- Digital Society/myapp/models.py
+++ Digital Society/myapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+	usertype=models.CharField(max_length=50, default="member")
+	house=models.CharField(max_length=50)
 	fname=models.CharField(max_length=100)
 	lname=models.CharField(max_length=100)
 	email=models.EmailField()
 	mobile=models.PositiveSmallIntegerField()
-	gender=models.CharField(max_length=50)
-	address=models.CharField(max_length=100)
 	password=models.CharField(max_length=100)
 
 	def __str__(self):
