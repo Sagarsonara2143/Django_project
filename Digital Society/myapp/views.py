35a35c19808aa673ac5e564e49c4fe42218ecbe9
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-31 23:13:52 +0530

    31.05.2023

--- Digital Society/myapp/views.py
+++ Digital Society/myapp/views.py
@@ -72,5 +72,6 @@ def login(request):
 	
 def member(request):
 	#User.objects.get(email=request.session['email'])
-	member=User.objects.get(usertype="member")
-	return render(request, 'member.html',{'member':member})
+	members=User.objects.get(usertype="member")
+	print(members)
+	return render(request, 'member.html',{'members':members})
