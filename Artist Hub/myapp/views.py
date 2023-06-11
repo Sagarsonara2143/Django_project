34e1ecc8ec84c5fedcfcfee2cea7c458cc6d15d5
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-06-11 11:55:54 +0530

    11.06.2023

--- Artist Hub/myapp/views.py
+++ Artist Hub/myapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-	return render(request,'index.html')
+	return render(request,'header.html')
+
+def about(request):
+	return render(request,'about-us.html')
