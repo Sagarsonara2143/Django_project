fd7ab7b65c5ef3d3274429dc04e308c0e99f5013
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-06-11 14:25:46 +0530

    11.06.2023

--- Artist Hub/myapp/models.py
+++ Artist Hub/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+	name=models.CharField(max_length=100)
+	email=models.EmailField()
+	mobile=models.PositiveSmallIntegerField()
+	message=models.TextField(max_length=500)
+
+	def __str__(self):
+		return self.name
